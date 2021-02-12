89547619c3023cfd5708ca17892bd70167d6c7c0
leonli0912 <623666429@qq.com>
2021-02-12 23:37:01 +0800

    refactor:seprate funcationality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+
+struct Config{
+    query:String,
+    filename:String,
+}  
+
+impl Config {
+    fn new(args:&[String]) -> Result<Config,&str>{
+        if args.len()<3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config {
+            query,
+            filename
+        })
+    }
+}
 
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+            println!("Problem parsing arguments:{}",err);
+            process::exit(1);
+    });
 
-    println!("Searching for {}",query);
-    println!("In file {}",filename);
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.filename);
 
-    let contents = fs::read_to_string(filename)
-    .expect("somthing went wrong on reading the file");
+    run(config);
+}
+
+fn run(config:Config) -> Result<(),Box<dyn Error>>{
+    let contents = fs::read_to_string(config.filename)?;
+    //.expect("somthing went wrong on reading the file");
 
     println!("write text:\n{}",contents);
+
+    Ok(())
 }

1aab3a67991e4a46c70c6699cfc4ef2429d9db97
leonli0912 <623666429@qq.com>
2021-02-15 15:02:59 +0800

    feature:search with env var

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config{
     pub query:String,
     pub filename:String,
+    pub case_sensitive:bool,
 }  
 
 impl Config {
@@ -14,10 +16,12 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     
         Ok(Config {
             query,
-            filename
+            filename,
+            case_sensitive,
         })
     }
 }
@@ -25,9 +29,14 @@ impl Config {
 pub fn run(config:Config) -> Result<(),Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
     //.expect("somthing went wrong on reading the file");
-    for line in search(&config.query,&contents){
+    let results = if config.case_sensitive {
+        search(&config.query,&contents)
+    }else{
+        search_case_insensitive(&config.query,&contents)
+    };
+    for line in results{
         println!("{}",line);
-    }
+    };
     Ok(())
 }
 
@@ -43,17 +52,39 @@ pub fn search<'a>(query:&str,contents:&'a str) -> Vec<&'a str>{
     results
 }
 
+pub fn search_case_insensitive<'a>(query:&str,contents:&'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+    }
+    
+    results
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
 
     #[test]
-    fn one_reuslt(){
+    fn case_sensitive(){
         let query = "duct";
         let contents = "\
         rust:safe,fast,productive.
         pcik three";
 
         assert_eq!(vec!["rust:safe,fast,productive."],search(query,contents));
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "dUCt";
+        let contents = "\
+        rust:safe,fast,productive.
+        pcik three";
+
+        assert_eq!(vec!["rust:safe,fast,productive."],search_case_insensitive(query,contents));
     }
 }

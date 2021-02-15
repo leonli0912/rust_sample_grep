a24f9691016988e25c8b98fda3959e2a39f6379b
leonli0912 <623666429@qq.com>
2021-02-15 13:48:29 +0800

    refactor:done

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
-
-struct Config{
-    query:String,
-    filename:String,
-}  
-
-impl Config {
-    fn new(args:&[String]) -> Result<Config,&str>{
-        if args.len()<3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config {
-            query,
-            filename
-        })
-    }
-}
+use rust_sample_grep::Config;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -34,14 +12,11 @@ fn main() {
     println!("Searching for {}",config.query);
     println!("In file {}",config.filename);
 
-    run(config);
+    if let Err(e) =
+        rust_sample_grep::run(config){
+            println!("Application error:{}",e);
+            process::exit(1);
+    };
 }
 
-fn run(config:Config) -> Result<(),Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-    //.expect("somthing went wrong on reading the file");
 
-    println!("write text:\n{}",contents);
-
-    Ok(())
-}
